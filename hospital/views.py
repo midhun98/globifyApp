999610ada745e223ec2e5df58c091756e58af2c7
midhun98 <midhunskani@gmail.com>
2024-05-28 23:42:47 +0530

    Initial

--- hospital/views.py
+++ hospital/views.py
@@ -12,6 +12,12 @@ class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patients.objects.all()
     serializer_class = PatientsSerializer
 
+    def get_queryset(self):
+        queryset = Patients.objects.all()
+        hospital_id = self.request.query_params.get('hospital', None)
+        if hospital_id is not None:
+            queryset = queryset.filter(hospital_id=hospital_id)
+        return queryset
 
 class HospitalViewset(viewsets.ModelViewSet):
     queryset = Hospital.objects.all()
